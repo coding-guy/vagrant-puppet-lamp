56f6e5fec861f027721ba8d29128c975a2362b52
Frank Stelzer <dev@frankstelzer.de>
2013-05-20 11:04:09 +0200

    removed hardcoded apache service dependency

--- modules/xdebug/manifests/init.pp
+++ modules/xdebug/manifests/init.pp
@@ -1,8 +1,10 @@
-class xdebug {
-    package { 'xdebug':
-        name    => 'php5-xdebug',
-        ensure  => installed,
-        require => Package['php'],
-        notify  => Service['apache']
-    }
+class xdebug (
+  $service = 'apache',
+){
+  package { 'xdebug':
+    name    => 'php5-xdebug',
+    ensure  => installed,
+    require => Package['php'],
+    notify  => Service[$service]
+  }
 }
